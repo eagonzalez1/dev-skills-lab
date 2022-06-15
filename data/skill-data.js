fc5a3177ab065711dac2c285e0b000329d260996
eagonzalez81 <gonzoatc81@gmail.com>
2022-06-15 18:04:41 -0400

    refactor

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+
+
+const skills = [
+  {text: 'HTML/CSS', got: true, _id: 125223},
+  {text: 'JS', got: true, _id: 127904},
+  {text: 'Backend Stuff', got: false, _id: 139608},
+]
+
+export { 
+	skills
+}
