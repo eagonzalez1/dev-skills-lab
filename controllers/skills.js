302f58635ff95dc5aa52fd037c5f9299a2ccce7c
eagonzalez81 <gonzoatc81@gmail.com>
2022-06-16 16:43:52 -0400

    add background color

--- controllers/skills.js
+++ controllers/skills.js
@@ -42,10 +42,22 @@ function show(req, res) {
   })
 }
 
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
 export {
   index,
   newSkill as new,
   create,
-  show
+  show,
+  deleteSkill as delete
 }
 
